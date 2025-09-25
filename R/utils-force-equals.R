4e361832b453efd136ea48e6256da8bc05554cf3
robchallen <rob@terminological.co.uk>
2025-09-25 17:27:51 +0100

    fix workflows

--- R/utils-force-equals.R
+++ R/utils-force-equals.R
@@ -9,16 +9,3 @@
 #' @importFrom rlang :=
 #' @usage x := y
 NULL
-
-
-#' If null operator
-#'
-#' See \code{\link[rlang:op-null-default]{rlang::`\%||\%`}} for details.
-#'
-#' @name %||%
-#' @rdname op-null-default
-#' @keywords internal
-#' @export
-#' @importFrom rlang %||%
-#' @usage x \%||\% default
-NULL
